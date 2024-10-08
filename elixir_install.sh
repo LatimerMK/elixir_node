54ad4d1deeb8e28b301f44545e05d7a7b199057a
LatimerMK <maxlatimer16@gmail.com>
2024-10-08 23:24:32 +0300

    first commit

--- elixir_install.sh
+++ elixir_install.sh
@@ -16,7 +16,7 @@ read -p "Введіть STRATEGY_EXECUTOR_BENEFICIARY (адресу каманц
 read -p "Введіть SIGNER_PRIVATE_KEY для $container_name (без 0x): " private_key
 
 # Записуємо дані у файл env
-cat <<EOL > $env_file
+cat <<EOL > "$env_file"
 ENV=testnet-3
 
 STRATEGY_EXECUTOR_DISPLAY_NAME=$display_name
@@ -26,10 +26,25 @@ EOL
 
 echo "Файл $env_file створено!"
 
-# Запускаємо Docker-контейнер
-docker run --env-file $HOME/elixir/$env_file --name $container_name --platform linux/amd64 --restart always -p $node_port:$node_port elixirprotocol/validator:v3
+# Перевіряємо, чи файл створено
+if [[ -f "$env_file" ]]; then
+    echo "Файл .env успішно створено."
+else
+    echo "Помилка: Файл .env не було створено."
+    exit 1
+fi
 
-echo "Docker-контейнер $container_name запущено!"
+# Запускаємо Docker-контейнер
+docker run --env-file "$env_file" --name "$container_name" --platform linux/amd64 --restart always -p "$node_port:$node_port" elixirprotocol/validator:v3
+
+# Перевіряємо, чи контейнер запущено
+if [ $? -eq 0 ]; then
+    echo "Docker-контейнер $container_name запущено!"
+else
+    echo "Помилка: Не вдалося запустити Docker-контейнер."
+    exit 1
+fi
+
 echo "Port $node_port"
 echo "MM $beneficiary "
 echo "Validator name $display_name"
