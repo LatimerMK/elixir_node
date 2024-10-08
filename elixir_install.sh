5b8207e60d97e8ed1f13b8e107c98341b62c9fe9
LatimerMK <maxlatimer16@gmail.com>
2024-10-09 00:30:35 +0300

    first commit

--- elixir_install.sh
+++ elixir_install.sh
@@ -5,7 +5,7 @@ read -p "Введіть ім'я Docker-контейнера: " container_name
 read -p "Введіть порт ноди сток (17690): " node_port
 
 # Створення директорії elixir, якщо її ще немає
-mkdir -p $HOME/elixir && cd $HOME/elixir
+mkdir -p "$HOME/elixir" && cd "$HOME/elixir"
 
 # Створюємо файл env з ім'ям контейнера
 env_file="${container_name}.env"
@@ -16,7 +16,7 @@ read -p "Введіть STRATEGY_EXECUTOR_BENEFICIARY (адресу каманц
 read -p "Введіть SIGNER_PRIVATE_KEY для $container_name (без 0x): " private_key
 
 # Записуємо дані у файл env
-cat <<EOL > $env_file
+cat <<EOL > "$env_file"
 ENV=testnet-3
 
 STRATEGY_EXECUTOR_DISPLAY_NAME=$display_name
@@ -26,15 +26,15 @@ EOL
 
 echo "Файл $env_file створено!"
 
+# Завантаження образу
 docker pull elixirprotocol/validator:v3 --platform linux/amd64
 
 echo "Port $node_port"
 echo "MM $beneficiary "
 echo "Validator name $display_name"
 echo "elixir/$env_file"
-echo "sudo docker run --env-file $HOME/elixir/$env_file --name $container_name --platform linux/amd64 --restart always -p $node_port:$node_port elixirprotocol/validator:v3"
+
 # Запускаємо Docker-контейнер
-sudo docker run -d --env-file /elixir/$env_file --name $container_name --platform linux/amd64 --restart always -p $node_port:$node_port elixirprotocol/validator:v3
+sudo docker run -d --env-file "$HOME/elixir/$env_file" --name "$container_name" --platform linux/amd64 --restart always -p "$node_port:$node_port" elixirprotocol/validator:v3
 
-
-
+echo "Docker-контейнер $container_name запущено!"
